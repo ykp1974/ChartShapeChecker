956bc5cc6ef3cbf4f90cc497d6db68a210a4f105
sikt_user_y.yamaguchi-at-150998627925 <y.yamaguchi[advan-school.jp>
2026-04-30 16:25:11 +0900

    2026/4/30@Adv シンプルマニュアル作成

--- doc/simple_manual.md
+++ doc/simple_manual.md
@@ -1,3 +1,8 @@
+■ChartShapeAnalyzer の使い方
+・python main_task.py の実行
+・Google Drive#dev#chart_pics に作られた古いフォルダ（退避画像）を削除
+
+■ChartShapeCheckerの使い方
 $env:GOOGLE_DRIVE_API_KEY="AIzaSyASbJFYKHulFnawb9v8Qt0011e-NRqnJ5A"; npm run update-tickers
 
 ※キーはGoogle Drive API Key取得手順.md を参照して確認
--- src/data/tickers.js
+++ src/data/tickers.js
@@ -1,37 +1,142 @@
 export const tickers = [
   {
-    symbol: "166A",
-    market: "T",
-    name: "タスキホールディングス",
-    id: "1FlYc4XEvX0pyYdiX1p6ap44SbiC1qS4q",
-    filename: "166A_T_タスキホールディングス_chart.png"
+    "symbol": "1356",
+    "market": "T",
+    "name": "ＴＯＰＩＸベア２倍上場投信",
+    "id": "13PuoMZxeUbPhQoZ934yDIVSUmX4OF_mz",
+    "filename": "1356_T_ＴＯＰＩＸベア２倍上場投信_chart.png"
   },
   {
-    symbol: "2217",
-    market: "T",
-    name: "モロゾフ",
-    id: "1OkYD4oqrY5zHJahJmtDtx-gbwnb1Qx33",
-    filename: "2217_T_モロゾフ_chart.png"
+    "symbol": "1569",
+    "market": "T",
+    "name": "ＴＯＰＩＸベア上場投信",
+    "id": "14cCIpQ33DP0prwbCaMappiknZg0uxcdx",
+    "filename": "1569_T_ＴＯＰＩＸベア上場投信_chart.png"
   },
   {
-    symbol: "4768",
-    market: "T",
-    name: "大塚商会",
-    id: "1Gpg96pxbmZhDWbkcW6AfDOCTifv4IplT",
-    filename: "4768_T_大塚商会_chart.png"
+    "symbol": "2212",
+    "market": "T",
+    "name": "山崎製パン",
+    "id": "1zLj9X_JohdDYBCQSTvjKeSs25_iBEmZz",
+    "filename": "2212_T_山崎製パン_chart.png"
   },
   {
-    symbol: "5729",
-    market: "T",
-    name: "日本精鉱",
-    id: "1fF8DEcEjygLLVLJ8fQuCoDsh441SIguF",
-    filename: "5729_T_日本精鉱_chart.png"
+    "symbol": "2282",
+    "market": "T",
+    "name": "日本ハム",
+    "id": "1ABK-8T_mNR3BF8bwQ7S2ZuvbxIakYsEq",
+    "filename": "2282_T_日本ハム_chart.png"
   },
   {
-    symbol: "8244",
-    market: "T",
-    name: "近鉄百貨店",
-    id: "1LFHwM0V7DFte0wCEAT3PuDp_6G-RE4u1",
-    filename: "8244_T_近鉄百貨店_chart.png"
+    "symbol": "2288",
+    "market": "T",
+    "name": "丸大食品",
+    "id": "1iAH3i6InhLHyjRUMr28WAL4SqIKh_cIc",
+    "filename": "2288_T_丸大食品_chart.png"
+  },
+  {
+    "symbol": "2674",
+    "market": "T",
+    "name": "ハードオフコーポレーション",
+    "id": "1unkwdHZgNedFvKMsnBrM0CM4vVkv1CSN",
+    "filename": "2674_T_ハードオフコーポレーション_chart.png"
+  },
+  {
+    "symbol": "3387",
+    "market": "T",
+    "name": "クリエイト・レストランツ・ホールディング",
+    "id": "1bR_f-dfIPoXaVY05svwBpu_5CaCRdLLe",
+    "filename": "3387_T_クリエイト・レストランツ・ホールディング_chart.png"
+  },
+  {
+    "symbol": "3632",
+    "market": "T",
+    "name": "グリーホールディングス",
+    "id": "1AcnL2rMvcBlFHDIcUsqPihLwZvVwmH3S",
+    "filename": "3632_T_グリーホールディングス_chart.png"
+  },
+  {
+    "symbol": "4072",
+    "market": "T",
+    "name": "電算システムホールディングス",
+    "id": "1MJB4V6QwAgDKJYiPpy9G8dvXIE3YRJHm",
+    "filename": "4072_T_電算システムホールディングス_chart.png"
+  },
+  {
+    "symbol": "4326",
+    "market": "T",
+    "name": "インテージホールディングス",
+    "id": "1T0gJeoNkAdE18NEAIy7zQcv5yp6HcbTz",
+    "filename": "4326_T_インテージホールディングス_chart.png"
+  },
+  {
+    "symbol": "4927",
+    "market": "T",
+    "name": "ポーラ・オルビスホールディングス",
+    "id": "1H10mC03RauesJLrQ5sUEUc4mFnWwpUQ_",
+    "filename": "4927_T_ポーラ・オルビスホールディングス_chart.png"
+  },
+  {
+    "symbol": "5970",
+    "market": "T",
+    "name": "ジーテクト",
+    "id": "1FnQIjns6C21kbzrBx8dJxRvby4GxiwSB",
+    "filename": "5970_T_ジーテクト_chart.png"
+  },
+  {
+    "symbol": "6625",
+    "market": "T",
+    "name": "ＪＡＬＣＯホールディングス",
+    "id": "1T_vPyNPJjL3VEDh2kr7Gz4TRGODg_TYr",
+    "filename": "6625_T_ＪＡＬＣＯホールディングス_chart.png"
+  },
+  {
+    "symbol": "6810",
+    "market": "T",
+    "name": "マクセル",
+    "id": "1-HJ_VKg5QHMfBU8OlaoJuocrSAZC0oti",
+    "filename": "6810_T_マクセル_chart.png"
+  },
+  {
+    "symbol": "7458",
+    "market": "T",
+    "name": "第一興商",
+    "id": "1PiUtqIajqqbrhyqJIcXY08aOe7coghnF",
+    "filename": "7458_T_第一興商_chart.png"
+  },
+  {
+    "symbol": "7846",
+    "market": "T",
+    "name": "パイロットコーポレーション",
+    "id": "1_SVkXxO4keTZvFehKMjvgWalF7NZ2sdn",
+    "filename": "7846_T_パイロットコーポレーション_chart.png"
+  },
+  {
+    "symbol": "7943",
+    "market": "T",
+    "name": "ニチハ",
+    "id": "1S4b-WcpZddhJKqJ-7n9c0DLT626Z-gU9",
+    "filename": "7943_T_ニチハ_chart.png"
+  },
+  {
+    "symbol": "8984",
+    "market": "T",
+    "name": "大和ハウスリート投資法人　投資証券",
+    "id": "1pjMU-woSBgAA_62LdtBIeb1AbK7alglD",
+    "filename": "8984_T_大和ハウスリート投資法人　投資証券_chart.png"
+  },
+  {
+    "symbol": "9037",
+    "market": "T",
+    "name": "ハマキョウレックス",
+    "id": "1RlbznlX5NZ-p3lomGXg8MAAQgoy28iAP",
+    "filename": "9037_T_ハマキョウレックス_chart.png"
+  },
+  {
+    "symbol": "9369",
+    "market": "T",
+    "name": "キユーソー流通システム",
+    "id": "17F5GUyUnRtsaO9qnIPboUyFeXuyUf_lT",
+    "filename": "9369_T_キユーソー流通システム_chart.png"
   }
 ];
