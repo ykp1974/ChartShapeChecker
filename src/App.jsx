e835a81c46b75062a9f18599cb9c5939e4f16d27
sikt_user_y.yamaguchi-at-150998627925 <y.yamaguchi[advan-school.jp>
2026-07-10 11:02:53 +0900

    20260710@Adv url5

--- src/App.jsx
+++ src/App.jsx
@@ -65,7 +65,7 @@ function App() {
       name: t.name
     }));
 
-    const GAS_URL = 'https://script.google.com/macros/s/AKfycbyO9HXk25u5euAb9lgyileNUf-hBLT8PdcyynyW8bUQwy2JilRD1c3tCIUReH-8nVw/exec';
+    const GAS_URL = 'https://script.google.com/macros/s/AKfycbzB2Do_vXP_kAWfL_2xjD-au0g87NFLkl4_HGjNLebeH8AtoJO38GKjCu7elRTRIIk/exec';
 
     try {
       const response = await fetch(GAS_URL, {
